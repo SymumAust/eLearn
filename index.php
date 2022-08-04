2e0ac7ffa091bf7d86d6916d64460fb5df4b3630
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-08-05 01:18:14 +0600

    added navbar

--- index.php
+++ index.php
@@ -4,32 +4,75 @@
 
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="project.css">
-    </head>
-    <body>
+	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+		<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+			<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+				<html>
+				<head>
+					<meta charset="utf-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<title></title>
+					<meta name="description" content="">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link rel="stylesheet" href="project.css">
 
-        <?php
+					<!-- Latest compiled and minified CSS -->
+					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+					<!-- Latest compiled JavaScript -->
+					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+				</head>
+				<body>
+
+					<div class="row">
+						<div class="col-md-12 ">
+							
+							<!-- Grey with black text -->
+							<nav class="navbar navbar-expand-sm bg-light navbar-light navbar navbar-expand-sm bg-dark navbar-dark">
+								<div class="container-fluid">
+									<ul class="navbar-nav" ">
+										<span style="font-size: 30px;>
+										<li class="nav-item">
+											<a class="nav-link active " id="navActive" href="#"  "><b>eLearn</b></a>
+										</li>
+									</span>
+
+										<li class="nav-item">
+											<a class="nav-link" href="#"><b></b></a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="#" ><b>Home</b></a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="#"><b>Courses</b></a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="#"><b>About</b></a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="#"><b>Log In</b></a>
+
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="#"><b>Sign Up</b></a>
+										</li>
+										
+									</ul>
+								</div>
+							</nav>
+
+							<!-- Black background with white text -->
+							
 
 
-    echo "";
-
-
-?>
-        <h1 class="head1">eLearn</h1>
+						</div>
+					</div>
+					<h1 class="head1">eLearn</h1>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
-        <script src="" async defer></script>
-    </body>
-</html>
+
+        	<script src="" async defer></script>
+        </body>
+        </html>
