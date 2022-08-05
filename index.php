0046f81d7fc3df6c943a2a210b3eee827e288088
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-08-05 07:07:36 +0600

    updated_final_Navbar

--- index.php
+++ index.php
@@ -22,53 +22,65 @@
 					<!-- Latest compiled JavaScript -->
 					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
+					<link rel="shortcut icon" href="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+
 				</head>
 				<body>
 
-					<div class="row">
-						<div class="col-md-12 ">
+
+					<div id="contentOfNav"class="row bg-secondary " style="box-shadow: 5px 5px 10px #09253d; color: #08233b;">
+
+						
+						<div class="col-md-4 ">
 							
-							<!-- Grey with black text -->
-							<nav class="navbar navbar-expand-sm bg-light navbar-light navbar navbar-expand-sm bg-dark navbar-dark">
+							
+							<nav class="navbar navbar-light ">
 								<div class="container-fluid">
-									<ul class="navbar-nav" ">
-										<span>
-										<li class="nav-item">
-											<a class="nav-link active " id="navActive" href="#"  "><b>eLearn</b></a>
-										</li>
-									</span>
-
-										<li class="nav-item">
-											<a class="nav-link" href="#"><b></b></a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link" href="#" ><b>Home</b></a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link" href="#"><b>Courses</b></a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link" href="#"><b>About</b></a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link" href="#"><b>Log In</b></a>
-
-										</li>
-										<li class="nav-item">
-											<a class="nav-link" href="#"><b>Sign Up</b></a>
-										</li>
-										
-									</ul>
+									<a class="navbar-brand" href="#">
+										<img src="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+										 eLearn</b>
+									</a>
 								</div>
 							</nav>
 
-							<!-- Black background with white text -->
-							
-
 
 						</div>
+						<div class="col-md-8" style="padding-left: 400px;">
+
+
+							<nav class="navbar navbar-expand-lg navbar-light " style="font-size:15px; padding-top: 15px;>
+								<div class="container-fluid">
+									<a class="navbar-brand" href="#"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
+									<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="navbar-toggler-icon"></span>
+									</button>
+									<div class="collapse navbar-collapse" id="navbarNav">
+										<ul class="navbar-nav">
+											<li class="nav-item">
+												<a class="nav-link active" aria-current="page" href="#" style="color: #7bb2e3"><b>Home</b></a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="#" style="color: #7bb2e3"><b>Team</b></a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="#" style="color: #7bb2e3"><b>About</b></a>
+											</li>
+
+												<li class="nav-item">
+												<a class="nav-link" href="#" style="color: #7bb2e3"><b>Contact</b></a>
+											</li>
+
+												<li class="nav-item">
+												<a class="nav-link" href="#" style="color: #7bb2e3"><b>Login</b></a>
+											</li>
+											
+										</ul>
+									</div>
+									</nav>
+								</div>
+							
 					</div>
-					<h1 class="head1">eLearn</h1>
+				<!--	<h1 class="head1">eLearn</h1> -->
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
