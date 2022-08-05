0f7b62b24e29bbbe5302c53ecb1ca6c8309efbaa
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-08-06 01:13:37 +0600

    login form added

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 	<title>eLearn</title>
 	<meta name="description" content="">
 
-
+	<link rel="stylesheet" type="text/css" href="login.css">
 
 	<!-- Latest compiled and minified CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -14,67 +14,91 @@
 	<!-- Latest compiled JavaScript -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="shortcut icon" href="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
-<!-- NavBar -->
+		<!-- NavBar -->
 
 		<div id="contentOfNav"class="row bg-secondary container-fluid" style="box-shadow: 5px 5px 10px #09253d; color: #08233b;">
 
 
 
 
-							<div class="col-md-4 ">
+			<div class="col-md-4 ">
 
 
-								<nav class="navbar navbar-light ">
-									<div class="container-fluid">
-										<a class="navbar-brand" href="#">
-											<img src="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
-											eLearn</b>
-										</a>
-									</div>
-								</nav>
+				<nav class="navbar navbar-light ">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="#">
+							<img src="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+							eLearn</b>
+						</a>
+					</div>
+				</nav>
 
 
-							</div>
-							<div class="col-md-8" style="padding-left: 400px;">
+			</div>
+			<div class="col-md-8" style="padding-left: 400px;">
 
 
-								<nav class="navbar navbar-expand-lg navbar-light " style="font-size:15px; padding-top: 15px;>
-								<div class="container-fluid">
-									<a class="navbar-brand" href="#"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
-									<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-										<span class="navbar-toggler-icon"></span>
-									</button>
-									<div class="collapse navbar-collapse" id="navbarNav">
-										<ul class="navbar-nav">
-											<li class="nav-item">
-												<a class="nav-link active" aria-current="page" href="index.php" style="color: #7bb2e3"><b>Home</b></a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="#" style="color: #7bb2e3"><b>Team</b></a>
-											</li>
-											<li class="nav-item">
-												<a class="nav-link" href="#" style="color: #7bb2e3"><b>About</b></a>
-											</li>
+				<nav class="navbar navbar-expand-lg navbar-light " style="font-size:15px; padding-top: 15px;>
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="index.php" style="color: #7bb2e3"><b>Home</b></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" style="color: #7bb2e3"><b>Team</b></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" style="color: #7bb2e3"><b>About</b></a>
+							</li>
 
-											<li class="nav-item">
-												<a class="nav-link" href="#" style="color: #7bb2e3"><b>Contact</b></a>
-											</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" style="color: #7bb2e3"><b>Contact</b></a>
+							</li>
 
-											<li class="nav-item">
-												<a class="nav-link" href="login.php" style="color: #7bb2e3"><b>Login</b></a>
-											</li>
-											
-										</ul>
-									</div>
-								</nav>
-							</div>
+							<li class="nav-item">
+								<a class="nav-link" href="login.php" style="color: #7bb2e3"><b>Login</b></a>
+							</li>
 
-						</div>
+						</ul>
+					</div>
+				</nav>
+			</div>
+
+		</div>
 
 	</head>
 	<body style="background-image: url(images/background.jpg); background-repeat: no-repeat;background-attachment: fixed ;background-size: cover;">
 
-		<div class="col pt-5 mt-5 ">
+		<div class="row col pt-5 mt-5 container-fluid ">
+
+			<div class="wrapper fadeInDown">
+				<div id="formContent">
+					<!-- Tabs Titles -->
+
+					<!-- Icon -->
+					<div class="fadeIn first">
+						<img src="images/logo.png" style="padding-bottom: 10px; padding-top: 15px;" id="icon" alt="User Icon" />
+					</div>
+
+					<!-- Login Form -->
+					<form>
+						<input type="text" id="login" class="fadeIn second" name="login" placeholder="Login">
+						<input type="text" id="" class="fadeIn third" name="login" placeholder="Password">
+						<input type="submit" class="fadeIn fourth" value="Log In">
+					</form>
+
+					<!-- Remind Passowrd -->
+					<div id="formFooter">
+						<a class="underlineHover" style="text-decoration: none; font-size: 22px" href="#">Forgot Password?</a>
+					</div>
+
+				</div>
+			</div>
 			
 		</div>
 
