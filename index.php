92b70422616f516b21439969bfc29896b0b166f4
MarziaMoni <190204114@aust.edu>
2022-08-12 00:55:57 +0600

    Available courses and contact us

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
 
 					<!-- Latest compiled and minified CSS -->
 					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
 
 					<!-- Latest compiled JavaScript -->
 					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -115,21 +117,11 @@
 
 							</div>
 
+            
+
 
 							<!-- About Section -->
-							<div>
-								
-
-							</div>
-
-
-							<!-- Contact Section -->
-
-							<div>
-								
-
-
-							</div>
+							
 
 
 							<!-- Login -->
@@ -137,5 +129,206 @@
 
 							<script src="project.js"></script>
 							<link rel="stylesheet" href="project.css">
-						</body>
-						</html>
+
+
+<!--course-->
+<!-- Page Content -->
+<div class="container">
+
+  <!-- Page Heading -->
+  <h1 class="head">
+     Available Courses
+  </h1>
+
+  <div class="row">
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/1.png" alt=""></a>
+        <div class="card-body">
+        <h4>JavaScript for Beginners</h4>
+          <p class="card-text">Learn javascript online and supercharge your web design with this Javascript for beginners training course.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/2.png" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Introduction to Web Development
+          </h4>
+          <p class="card-text">Are you Interested in Learning Web Development? Enroll in this Free course for a Dynamic Introduction to the Profession!</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/3.jpg" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Learn Microsoft Excel 2013-Advanced
+          </h4>
+          <p class="card-text" >Insider Method: Learn Advanced Techniques for Microsoft Excel 2013 Fast - Without Lectures, Books, or Night Classes</p>
+            
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/4.png" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Learn Python
+          </h4>
+          <p class="card-text" >Python For Beginners : This course is meant for absolute beginners in programming or in python.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/5.png" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Data Analysis
+          </h4>
+          <p class="card-text" >Business Analytics will help demystify data and strengthen your analytical skills.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/6.png" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Organizational leadership
+          </h4>
+          <p class="card-text" >Learn how to set and communicate direction, influence through other managers, generate organizational alignment, drive innovation, and engineer change</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/7.jpg" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Design Thinkiing and Innovation
+          </h4>
+          <p class="card-text" >Uncover innovation solution to your most challenging business problems.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/8.jpeg" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Sustainable Business Strategy
+          </h4>
+          <p class="card-text" >Develop the tools and techniques to become a purpose-driven business.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/9.jpg" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+		  Financial Accounting
+          </h4>
+          <p class="card-text" >Discover what's behind the numbers in financial statements
+            </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+
+
+</div>
+<!-- /.container -->
+
+
+<!-- Contact -->
+
+ <div class="container">
+
+  <div class="header">
+    <h2>get in touch<h2>
+  </div>
+  <div class="content-form">
+    <section>
+    <i class="fa fa-location-arrow fa-2x" aria-hidden="true"></i>
+      <h2>Address</h2>
+      <p>
+        lorrem klsikjdmfk.<br>
+        lorrem klsikjdmfk.<br>
+        lorem.
+      </p>
+    </section>
+
+    <section>
+    <i class="fa fa-phone-square fa-2x" aria-hidden="true"></i>
+      <h2>Phoone</h2>
+      <p>1673893020</p>
+    </section>
+
+    <section>
+    <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+    <h2>E-mail</h2>
+      <p>lorem@gamil.com</p>
+    </section>
+  </div>
+
+  <form>
+    <div class="form">
+      <div class="right">
+
+        <div class="contact-form">
+          <input type="text" required>
+          <span>Full name</span>
+        </div>
+
+        <div class="contact-form">
+          <input type="E-mail" required>
+          <span>Email</span>
+        </div>
+
+        <div class="contact-form">
+          <textarea name="text"></textarea>
+          <span>
+            type your message
+          </span>
+        </div>
+
+        <div class="contact-form">
+          <input type="submit" name="submit">
+         
+        </div>
+
+
+
+      </div>
+    </div>
+  </form>
+
+  <div class="media">
+
+  <li><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></li>
+  <li><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></li>
+  <li><i class="fa fa-whatsapp fa-2x" aria-hidden="true"></i></li>
+  <li><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></li>
+  </div> 
+
+  <div class="empty">
+
+  
+  </div>
+</div>
+			
+</body>
+</html>
