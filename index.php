ce1be28e7afb4d7997a14fbfb0d21183e758dfb6
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-08-12 12:47:28 +0600

    courses Organized
    Partial work done

--- index.php
+++ index.php
@@ -20,11 +20,17 @@
 					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 					<!-- Latest compiled JavaScript -->
+
+          <link rel="stylesheet" type="text/css" href="project.css">
 					<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 					<link rel="shortcut icon" href="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <script src="project.js"></script>
 
 						
 <!-- Navbar -->
@@ -53,7 +59,7 @@
 
 								<nav class="navbar navbar-expand-lg navbar-light " style="font-size:15px; padding-top: 15px;>
 								<div class="container-fluid">
-									<a class="navbar-brand" href="#"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
+									<a class="navbar-brand" href="#course"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
 									<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 										<span class="navbar-toggler-icon"></span>
 									</button>
@@ -132,128 +138,22 @@
 
 
 <!--course-->
-<!-- Page Content -->
-<div class="container">
+<!-- Page Content 
+<div class="container pt-5 mt-5">
 
-  <!-- Page Heading -->
-  <h1 class="head">
-     Available Courses
-  </h1>
+  <!-- Page Heading 
+ 
 
-  <div class="row">
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/1.png" alt=""></a>
-        <div class="card-body">
-        <h4>JavaScript for Beginners</h4>
-          <p class="card-text">Learn javascript online and supercharge your web design with this Javascript for beginners training course.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/2.png" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Introduction to Web Development
-          </h4>
-          <p class="card-text">Are you Interested in Learning Web Development? Enroll in this Free course for a Dynamic Introduction to the Profession!</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/3.jpg" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Learn Microsoft Excel 2013-Advanced
-          </h4>
-          <p class="card-text" >Insider Method: Learn Advanced Techniques for Microsoft Excel 2013 Fast - Without Lectures, Books, or Night Classes</p>
-            
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/4.png" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Learn Python
-          </h4>
-          <p class="card-text" >Python For Beginners : This course is meant for absolute beginners in programming or in python.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/5.png" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Data Analysis
-          </h4>
-          <p class="card-text" >Business Analytics will help demystify data and strengthen your analytical skills.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/6.png" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Organizational leadership
-          </h4>
-          <p class="card-text" >Learn how to set and communicate direction, influence through other managers, generate organizational alignment, drive innovation, and engineer change</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/7.jpg" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Design Thinkiing and Innovation
-          </h4>
-          <p class="card-text" >Uncover innovation solution to your most challenging business problems.
-            </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/8.jpeg" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Sustainable Business Strategy
-          </h4>
-          <p class="card-text" >Develop the tools and techniques to become a purpose-driven business.
-            </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="images/9.jpg" alt=""></a>
-        <div class="card-body">
-        <h4 class="card-title">
-		  Financial Accounting
-          </h4>
-          <p class="card-text" >Discover what's behind the numbers in financial statements
-            </p>
-        </div>
-      </div>
-    </div>
   </div>
-  <!-- /.row -->
+  
+  <!-- /.row 
 
 
 </div>
-<!-- /.container -->
+<!-- /.container 
 
 
-<!-- Contact -->
+<!-- Contact 
 
  <div class="container">
 
@@ -328,7 +228,297 @@
 
   
   </div>
+
 </div>
+--->
+
+
+
+<div class="container pt-5 mt-5 " id="c1">
+  
+  <span id="course"></span>
+  <div class="row">
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/1.png" style="height: 300px;" alt=""></a>
+        <div class="card-body" id="">
+        <h4 style="font-size: 30px;">JavaScript for Beginners</h4>
+          <p class="card-text">Learn javascript online and supercharge your web design with this Javascript for beginners training course.</p>
+
+
+          <div class="row">
+
+            <div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+            <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 29$</a></div>
+
+        <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="font-size: 15px; "></span>
+<span class="fa fa-star" style="font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/2.png" style="height: 300px;" alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Introduction to Web Development
+          </h4>
+          <p class="card-text">Are you Interested in Learning Web Development? Enroll in this Free course for a Dynamic Introduction to the Profession!</p>
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 49$</a></div>
+           <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange; font-size: 15px; "></span>
+<span class="fa fa-star" style="font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/3.jpg" style="height: 300px; alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Learn Microsoft Excel 2013-Advanced
+          </h4>
+          <p class="card-text" >Insider Method: Learn Advanced Techniques for Microsoft Excel 2013 Fast - Without Lectures, Books, or Night Classes</p>
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 5$</a></div>
+
+        <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+
+            
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/4.png" style="height: 300px; alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Learn Python
+          </h4>
+          <p class="card-text" >Python For Beginners : This course is meant for absolute beginners in programming or in python.</p>
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 99$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: ; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: ; font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/5.png" style="height: 300px; alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Data Analysis
+          </h4>
+          <p class="card-text" >Business Analytics will help demystify data and strengthen your analytical skills.</p>
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 49$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: ; font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/6.png" style="height: 300px; alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Organizational leadership
+          </h4>
+          <p class="card-text" >Learn how to set and communicate direction, influence through other managers, generate organizational alignment, drive innovation, and engineer change</p>
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+ 
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 29$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: ; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: ; font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/7.jpg" style="height: 300px;alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Design Thinkiing and Innovation
+          </h4>
+          <p class="card-text" >Uncover innovation solution to your most challenging business problems.
+            </p>
+
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 99$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: ;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: ; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: ; font-size: 15px;"></span>
+
+           </div></div>
+
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/8.jpeg" style="height: 300px;alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Sustainable Business Strategy
+          </h4>
+          <p class="card-text" >Develop the tools and techniques to become a purpose-driven business.
+            </p>
+
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div> 
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 49$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: ; font-size: 15px;"></span>
+
+           </div></div>
+
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 mb-4">
+      <div class="card h-100">
+        <a href="#"><img class="card-img-top" src="images/9.jpg" style="height: 300px; alt=""></a>
+        <div class="card-body">
+        <h4 class="card-title">
+      Financial Accounting
+          </h4>
+          <p class="card-text" >Discover what's behind the numbers in financial statements
+            </p>
+
+
+           <div class="row"><div class="col-md-6">
+            <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+          </div>
+
+          <div class="col-md-6">          <a href="" style="font-size: 25px;">Price 9$</a></div>
+          <div class="col-12" style="padding-left: 20px; align-content: right; justify-content:right;"><div class="col-md-4" id="star">
+          <span></span>
+          <span class="fa fa-star checked"style="color: orange; font-size: 15px;"></span>
+<span class="fa fa-star checked" style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star checked"style="color: orange;font-size: 15px;"></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px; "></span>
+<span class="fa fa-star" style="color: orange; font-size: 15px;"></span>
+
+           </div></div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+<script src="card.js"></script>
+
+
+
+
+
 			
 </body>
 </html>
