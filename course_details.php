67d64df51cce84b9b025dd59b9bb2f2ca581aa42
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-08-18 07:01:04 +0600

    course organized

--- course_details.php
+++ course_details.php
@@ -4,99 +4,217 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eLearn</title>
+
     <link rel="stylesheet" href="course_details.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    <link rel="shortcut icon" href="images/logo.png" alt="logo" width="30" height="45" id="oop">
+
+    <div id="contentOfNav"class="row bg-secondary container-fluid" style="box-shadow: 5px 5px 10px #09253d; color: #08233b; ">
+
+
+
+
+       <div class="col-md-4 ">
+
+
+          <nav class="navbar navbar-light ">
+             <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                   <img src="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+                   eLearn</b>
+               </a>
+           </div>
+       </nav>
+
+
+   </div>
+   <div class="col-md-8" style="padding-left: 400px;">
+
+
+      <nav class="navbar navbar-expand-lg navbar-light " style="font-size:15px; padding-top: 15px;">
+          <div class="container-fluid">
+             <a class="navbar-brand" href="#st"><button type="button" class="btn btn-primary" color: #7bb2e3><b>Get Started</b></button></a>
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+             <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php" style="color: #7bb2e3"><b style="color:greenyellow;">Home</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #7bb2e3"><b>Team</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #7bb2e3"><b>About</b></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #7bb2e3"><b>Contact</b></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php" style="color: #7bb2e3"><b>Login</b></a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+</div>
+
+</div>
+
 
 </head>
-<body id="main" >
+<body id="main" style=""  >
 
- <section class="main container-fluid" >
+   <section class="main container-fluid " >
     <div class="c-head">
-        <h1>Course Details</h1>
+        <h1><b  style="text-decoration: overline;">Course Details</b></h1>
     </div>
-     <div class="container">
-        <div class="row ">
+    <div class="container">
+        <div id="row" class="row ">
             <div class="col">
                 <img src="images/1.png" alt="java" >
             </div>
             <div class="col">
                 <h2>JavaScript for Beginners</h2>
-    
+
                 <p>Learn javascript online and supercharge your web design with this Javascript for beginners training course</p>
                 <i class="bi bi-check" style="background: transparent;"></i><h6>Extensive, informative and interesting video lecture</h6>
                 <i class="bi bi-check" style="background: transparent;"></i><h6>Complete Code demonstrated in lecture</h6><br>
 
                 
-                <div class="row ">
-                <div class="col-md-6">
-          <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
-        </div>
-        <div class="col-md-6 ">
-             <span id="myspan"><b>$23</b></span>
-        </div></div>
+                <div id="row" class="row ">
+                    <div class="col-md-6">
+                      <button id="button" style="padding: 8px 35px; font-size: 20px; color: ;"><b> Enroll</b></button>
+                  </div>
+                  <div class="col-md-6 ">
+                   <span id="myspan"><b>$23</b></span>
+               </div></div>
                
-                
-                
-            </div>
-        </div>
 
-        
-     </div>
 
-     <div class="container-fluid">
-        <div class="row" >
-            <ul>
-                <li>
-                    <span class="num">1.</span>
-                    <span class="name">Introduction</span>
-                    <span style="margin-left:340px ;"><i class="bi bi-clock"  style="background: transparent;"></i></span>
-                    <span class="number">12:00</span>
-                    
-                </li>
-                <li>
-                    <span class="num">2.</span>
-                    <span class="name">Learn about variables,keyword,library functions</span>
-                    <span ><i class="bi bi-clock" style="background: transparent;"></i></span>
-                    <span class="number" style="margin-right:45px ;">15:00</span>
-                    
-                </li>
-                <li>
-                    <span class="num">3.</span>
-                    <span class="name">Learn about loops,conditional statememt
-                    </span>
-                    <span style="margin-left: 138px ;"><i class="bi bi-clock" style="background: transparent;"></i></span>
-                    <span class="number">20:00</span>
-                    
-                </li>
-                
-                    <li>
-                        <span class="num">4.</span>
-                        <span class="name">Number method</span>
-                        <span style="margin-left:314px ;"><i class="bi bi-clock" style="background: transparent;"></i></span>
-                        <span class="number">25:00</span>
-                    </li>
-               
-                
-                
+           </div>
+       </div>
+
+
+   </div>
+
+   <div class="container-fluid">
+    <div id="row" class="row" >
+        <ul>
+            <li>
+                <span class="num">1.</span>
+                <span class="name">Introduction</span>
+                <span style="margin-left:340px ;"><i class="bi bi-clock"  style="background: transparent;"></i></span>
+                <span class="number">12:00</span>
+
+            </li>
+            <li>
+                <span class="num">2.</span>
+                <span class="name">Learn about variables,keyword,library functions</span>
+                <span ><i class="bi bi-clock" style="background: transparent;"></i></span>
+                <span class="number" style="margin-right:45px ;">15:00</span>
+
+            </li>
+            <li>
+                <span class="num">3.</span>
+                <span class="name">Learn about loops,conditional statememt
+                </span>
+                <span style="margin-left: 138px ;"><i class="bi bi-clock" style="background: transparent;"></i></span>
+                <span class="number">20:00</span>
+
+            </li>
+
+            <li>
+                <span class="num">4.</span>
+                <span class="name">Number method</span>
+                <span style="margin-left:314px ;"><i class="bi bi-clock" style="background: transparent;"></i></span>
+                <span class="number">25:00</span>
+            </li>
+
+
+
+        </ul>
+
+        <div style="padding-bottom: 50px; font-family:  'DM Sans', sans-serif; justify-content: center; text-align: center; margin-right: 100px; font-weight: 500;" class="container-fluid mbr-section-btn align-left">
+                    <a class="btn btn-md btn-primary display-4" href="https://mobirise.com">
+                        Sign In for Excustion</a>
+                    <a style="background-color: ; color: white;" class="btn btn-md btn-danger display-4" href="https://mobirise.com">
+                        Learn More</a>
+                </div>
+    </div>
+
+</div>
+
+
+</section>
+
+<!-- Footer -->
+
+<div>
+  <footer>
+    <div class="footer-clean">
+      <footer>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-sm-4 col-md-3 item">
+              <h3>Services</h3>
+              <ul>
+                <li><a href="#">Web design</a></li>
+                <li><a href="#">Development</a></li>
+                <li><a href="#">Hosting</a></li>
             </ul>
-
-            <h3 class="container-fluid" style="text-align: center;">Purchase the course to continue...</h3>
         </div>
+        <div class="col-sm-4 col-md-3 item">
+          <h3>About</h3>
+          <ul>
+            <li><a href="#">Company</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Legacy</a></li>
+        </ul>
+    </div>
+    <div class="col-sm-4 col-md-3 item">
+      <h3>Careers</h3>
+      <ul>
+        <li><a href="#">Job openings</a></li>
+        <li><a href="#">Employee success</a></li>
+        <li><a href="#">Benefits</a></li>
+    </ul>
+</div>
+<div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+  <p class="copyright">Claimed by eLearn © 2022</p>
+</div>
+</div>
+</div>
+</footer>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 
-     </div>
-
-     
- </section>
+</footer>
+</div>
 
 
 
-    
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" type="text/css" href="style.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 </body>
 </html>
