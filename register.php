a5fe94161128cf86b5dfc6ffbbaab33f2a7db5ac
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-09-01 04:52:28 +0600

    Revert "Revert "forget password updated""
    This reverts commit 6597dea2fcd4d4089ab3b16caec669244efe09d4.

--- register.php
+++ register.php
@@ -1,46 +1,78 @@
+<?php session_start(); ?>
 <?php
     include('connect/connection.php');
 
-    if(isset($_POST["login"])){
-        $email = mysqli_real_escape_string($connect, trim($_POST['email']));
-        $password = trim($_POST['password']);
+    if(isset($_POST["register"])){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-        $sql = mysqli_query($connect, "SELECT * FROM user_table where email = '$email'");
-        $count = mysqli_num_rows($sql);
+        $check_query = mysqli_query($connect, "SELECT * FROM user_table where email ='$email'");
+        $rowCount = mysqli_num_rows($check_query);
 
-            if($count > 0){
-                $fetch = mysqli_fetch_assoc($sql);
-                $hashpassword = $fetch["password"];
+        if(!empty($email) && !empty($password)){
+            if($rowCount > 0){
+                ?>
+                <script>
+                    alert("User with email already exist!");
+                </script>
+                <?php
+            }else{
+                $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+                $result = mysqli_query($connect, "INSERT INTO login (email, password, status) VALUES ('$email', '$password_hash', 0)");
     
-                if($fetch["status"] == 0){
-                    ?>
-                    <script>
-                        alert("Please verify email account before login.");
-                    </script>
-                    <?php
-                }else if(password_verify($password, $hashpassword)){
-                    ?>
-                    <script>
-                        alert("login in successfully");
-                    </script>
-                    <?php
-                }else{
-                    ?>
-                    <script>
-                        alert("email or password invalid, please try again.");
-                    </script>
-                    <?php
+                if($result){
+                    $otp = rand(100000,999999);
+                    $_SESSION['otp'] = $otp;
+                    $_SESSION['mail'] = $email;
+                    require "Mail/phpmailer/PHPMailerAutoload.php";
+                    $mail = new PHPMailer;
+    
+                    $mail->isSMTP();
+                    $mail->Host='smtp.gmail.com';
+                    $mail->Port=587;
+                    $mail->SMTPAuth=true;
+                    $mail->SMTPSecure='tls';
+    
+                    $mail->Username='email account';
+                    $mail->Password='email password';
+    
+                    $mail->setFrom('email account', 'OTP Verification');
+                    $mail->addAddress($_POST["email"]);
+    
+                    $mail->isHTML(true);
+                    $mail->Subject="Your verify code";
+                    $mail->Body="<p>Dear user, </p> <h3>Your verify OTP code is $otp <br></h3>
+                    <br><br>
+                    <p>With regrads,</p>
+                    <b>Programming with Lam</b>
+                    https://www.youtube.com/channel/UCKRZp3mkvL1CBYKFIlxjDdg";
+    
+                            if(!$mail->send()){
+                                ?>
+                                    <script>
+                                        alert("<?php echo "Register Failed, Invalid Email "?>");
+                                    </script>
+                                <?php
+                            }else{
+                                ?>
+                                <script>
+                                    alert("<?php echo "Register Successfully, OTP sent to " . $email ?>");
+                                    window.location.replace('verification.php');
+                                </script>
+                                <?php
+                            }
                 }
             }
-                
+        }
     }
 
 ?>
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 <!------ Include the above in your HEAD tag ---------->
 
 <!doctype html>
@@ -61,13 +93,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-    <title>Login Form</title>
+    <title>Register Form</title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="#">Login Form</a>
+        <a class="navbar-brand" href="#">Register Form</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -75,10 +107,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php" style="font-weight:bold; color:black; text-decoration:underline">Login</a>
+                    <a class="nav-link" href="index.php" >Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="register.php">Register</a>
+                    <a class="nav-link" href="register.php" style="font-weight:bold; color:black; text-decoration:underline">Register</a>
                 </li>
             </ul>
 
@@ -91,9 +123,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form action="#" method="POST" name="login">
+                        <form action="#" method="POST" name="register">
                             <div class="form-group row">
                                 <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
                                 <div class="col-md-6">
@@ -109,21 +141,8 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="col-md-6 offset-md-4">
-                                <input type="submit" value="Login" name="login">
-                                <a href="recover_psw.php" class="btn btn-link">
-                                    Forgot Your Password?
-                                </a>
+                                <input type="submit" value="Register" name="register">
                             </div>
                     </div>
                     </form>
