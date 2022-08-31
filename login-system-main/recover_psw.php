8dd7253db0f5524353f9b88fd7716a599bbcd8e2
MD. SYMUM HOSSAIN <190204105@aust.edu>
2022-09-01 05:04:34 +0600

    Revert "Revert "Revert "forget password updated"""
    This reverts commit a5fe94161128cf86b5dfc6ffbbaab33f2a7db5ac.

--- login-system-main/recover_psw.php
+++ login-system-main/recover_psw.php
@@ -36,35 +36,158 @@
     </div>
 </nav>
 
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Password Recover</div>
-                    <div class="card-body">
-                        <form action="#" method="POST" name="recover_psw">
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
-                                </div>
-                            </div>
 
-                            <div class="col-md-6 offset-md-4">
-                                <input type="submit" value="Recover" name="recover">
-                            </div>
+   
+
+ 
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta charset="utf-8">
+   
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>eLearn</title>
+    <meta name="description" content="">
+
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+
+
+<!-- icon -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="shortcut icon" href="images/logo.png" alt="logo" width="30" height="45" style="padding-bottom: 12px;" class=""><b style="font-size: 30px; color: #7bb2e3">
+        <!-- NavBar -->
+        <span id="login"></span>
+
+
+    </head>
+    <body style="background-image: url(images/background.jpg); background-repeat: no-repeat;background-attachment: fixed ;background-size: cover;">
+
+
+        
+
+
+
+  <script>  
+            function validation()  
+            {  
+                var id=document.f1.email.value;  
+                
+                if(id.length=="" ) {  
+                    alert("Email is empty");  
+                    return false;  
+                }  
+                                          
+            }  
+        </script>  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Forget -->
+
+
+
+<span id="forget"></span>
+<div style="background-image: url(images/background.jpg); background-repeat: no-repeat;background-attachment: fixed ;background-size: cover; padding-bottom: 20px;" id="container">
+<div class="row col pt-5 mt-5  container-fluid " >
+
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <!-- Tabs Titles -->
+
+                    <!-- Icon -->
+                    <div class="fadeIn first">
+                        <img src="images/logo.png" style="padding-bottom: 10px; padding-top: 15px;" id="icon" alt="User Icon" />
                     </div>
+                    <h5>Reset Your Password</h5>
+
+                    <!-- Login Form -->
+                    <form action="#" method="POST" name="recover_psw" action="forget.php" name="f1" autocomplete="off" onsubmit = "return validation()" method = "POST">
+                        <?php 
+                            if($errors>0){
+                                foreach ($errors as $displayErrors) {
+                                    // code...
+                                    ?>
+                                    <div id="alert"><?php echo $displayErrors; ?></div>
+                                    <?php 
+
+                                }
+                            }
+                        ?>
+                        <input   type="text" id="email_address" class="form-control" name="email" required autofocus type="text" id="login" type="email" class="fadeIn second" name="email" placeholder="E-mail" style="font-size: 15px; justify-content: center; width: 85%; text-align: center; height: auto; padding-bottom: 12px; padding-top: 12px; margin-bottom: 20px; border-style: unset; box-shadow: 1px 1px 1px skyblue; border-color: skyblue; border-radius: 3px;">
+                        
+                        <input type="submit" value="Recover" name="recover" type="submit" name="forget_password"  class="fadeIn fourth" value="Check">
                     </form>
+                    <p style=" color: green ;font-size: 15px; font-weight: 300;">Enter your email address and we'll send you an email with instructions to reset your password.</p>
+
+                    <!-- Remind Passowrd -->
+                    <div id="formFooter">
+                        <a class="underlineHover" style="text-decoration: none; font-size: 15px" href="login.php">Log In?</a>
+                        <p class=""style="text-decoration: none; font-size: 15px">Doesn't have an account?  <a class="underlineHover" style="text-decoration: none; font-size: 15px" href="registration.php">Register Here.</a></p>
+                    </div>
+
+            
+
                 </div>
             </div>
+            
         </div>
-    </div>
-    </div>
 
-</main>
-</body>
-</html>
+    </div>
+        
+
+
+<?php include 'navbar.php' ?>
+<?php include 'footer.php' ?>
+
+<link rel="stylesheet" type="text/css" href="footer.css">
+            
+
+
+    </body>
+    </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <?php 
     if(isset($_POST["recover"])){
